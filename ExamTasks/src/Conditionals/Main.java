45e49f55509e93ca292275a776b87d105929fb1e
Ivaylo Dinev <201216004@fdiba.tu-sofia.bg>
2018-09-30 15:19:29 +0300

    Changed structure of solution

---
+++ ExamTasks/src/Conditionals/Main.java
@@ -0,0 +1,32 @@
+package Conditionals;
+import java.text.DecimalFormat;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#.##");
+        int a;
+        a = sc.nextInt();
+        if(a <= 0 || a > 9) {
+            System.out.println("invalid score");
+        }
+        else {
+            switch ((a - 1) / 3) {
+                case 0:
+                    a *= 10;
+                    break;
+                case 1:
+                    a *= 100;
+                    break;
+                case 2:
+                    a *= 1000;
+                    break;
+            }
+            System.out.println(a);
+        }
+    }
+}
